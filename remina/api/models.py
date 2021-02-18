6a1bc3d7f5c72173dd958f4773504daf6cc46d72
Romy Maghsoudi <romy.mag9@gmail.com>
2021-02-17 22:05:09 -0500

    basic planet model

--- remina/api/models.py
+++ remina/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Planet(model.Model):
+    description = models.CharField(max_length=140)
+    relative = models.IntegerField(default=None)
+    # TODO: insert custom validator for the type
+    type = models.CharField(max_length=10)
+    user = models.IntegerField(default=None)
+    xp = models.IntegerField(default=1)
+    completed = models.BooleanField(default=False)
+    dateCompleted = models.DateTimeField(auto_now=False, auto_now_add=False)
+    dueDate = models.DateTimeField(auto_now=False, auto_now_add=False)
+    # TODO: create custom validators for timePeriod and frequency
+    timePeriod = models.CharField(max_length=40)
+    frequency = models.CharField(max_length=40)
+    hId = models.IntegerField(default=None)
+
+# description, relative, type, user, xp, completed
+# dateCompleted, dueDate, timePeriod, frequency, hId
