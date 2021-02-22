217f2f750fa95bc24dff1176ba9c3f5d62416ba1
Romy Maghsoudi <romy.mag9@gmail.com>
2021-02-22 15:42:08 -0500

    divided overarching Planet model into multiple models for efficiency and added User model

--- remina/api/models.py
+++ remina/api/models.py
@@ -3,20 +3,70 @@ from django.db import models
 # Create your models here.
 
 
-class Planet(models.Model):
+class Goal(models.Model):
     description = models.CharField(max_length=140)
-    relative = models.IntegerField(default=None)
-    # TODO: insert custom validator for the type
-    type = models.CharField(max_length=10)
-    user = models.IntegerField(default=None)
+    relative = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
     xp = models.IntegerField(default=1)
     completed = models.BooleanField(default=False)
     dateCompleted = models.DateTimeField(auto_now=False, auto_now_add=False)
     dueDate = models.DateTimeField(auto_now=False, auto_now_add=False)
-    # TODO: create custom validators for timePeriod and frequency
+    # TODO: create custom validator for timePeriod
     timePeriod = models.CharField(max_length=40)
-    frequency = models.CharField(max_length=40)
-    hId = models.IntegerField(default=None)
 
-# description, relative, type, user, xp, completed
-# dateCompleted, dueDate, timePeriod, frequency, hId
+
+class Habit(models.Model):
+    description = models.CharField(max_length=140)
+    relative = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    xp = models.IntegerField(default=1)
+    completed = models.BooleanField(default=False)
+    dateCompleted = models.DateTimeField(auto_now=False, auto_now_add=False)
+    # TODO: create custom validators for frequency
+    frequency = models.CharField(max_length=40)
+
+
+class Todo(models.Model):
+    description = models.CharField(max_length=140)
+    relative = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    xp = models.IntegerField(default=1)
+    completed = models.BooleanField(default=False)
+    dateCompleted = models.DateTimeField(auto_now=False, auto_now_add=False)
+    dueDate = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+class User(models.Model):
+    username = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    level = models.IntegerField(
+        default=1,
+    )
+    xp_total = models.IntegerField(
+        default=0,
+    )
+    xp_to_lvlup = models.IntegerField(
+        default=1515,
+    )
+
+# attributes for the user class: an identifier (email, username, etc..), level,
