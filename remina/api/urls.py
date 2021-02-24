3a1f42c9fba9681c93d4b8d00f2093c6aa85a24c
Romy Maghsoudi <romy.mag9@gmail.com>
2021-02-23 22:36:12 -0500

    added url for users/

--- remina/api/urls.py
+++ remina/api/urls.py
@@ -4,5 +4,6 @@ from .views import UserView
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('users/', UserView.as_view()),
     path('users/<int:pk>', UserView.as_view()),
 ]
