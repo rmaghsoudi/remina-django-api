36fc44d72df1599ea751a7d8d758b08e34db7b82
Romy Maghsoudi <romy.mag9@gmail.com>
2021-03-01 16:46:41 -0500

    adding leveling logic to user model

--- remina/api/models.py
+++ remina/api/models.py
@@ -90,5 +90,51 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+    
+    # leveling formula, using the current level it determines how much is needed to lvlup
+    def calculate_xp_to_lvlup(self, level=None):
+        if level == None:
+            xp_needed = 5 * (self.level ^ 2) + 500 * self.level + 1000
+        else:
+            xp_needed = 5 * (level ^ 2) + 500 * level + 1000
+        return xp_needed
 
-# attributes for the user class: an identifier (email, username, etc..), level,
+    # calculates when xp can level up user more than once
+    def multiple_lvlup(self, xp):
+        running = True
+        xp = xp
+        level = self.level + 1
+
+        while running:
+            if xp >= self.calculate_xp_to_lvlup(level):
+                xp -= self.calculate_xp_to_lvlup(level)
+                level += 1
+            else:
+                running = False
+        return xp, level
+
+    # handles level logic when xp is sent in a request
+    def leveling_up(self, xp):
+      # build new object because serializer doesn't accept User objects
+        user = model_to_dict(self)
+        xp_to_add = xp
+        xp_needed = user['xp_to_lvlup']
+        new_xp = user['xp'] + xp_to_add
+        xp_diff = new_xp - xp_needed
+
+        if xp_diff >= self.calculate_xp_to_lvlup(self.level + 1):
+            new_xp, new_level = self.multiple_lvlup(xp_diff)
+            user['level'] = new_level
+            user['xp'] = new_xp
+            user['xp_to_lvlup'] = self.calculate_xp_to_lvlup(new_level)
+
+        elif new_xp >= xp_needed:
+            user['level'] += 1
+            user['xp'] = xp_diff
+            user['xp_to_lvlup'] = self.calculate_xp_to_lvlup(user['level'])
+
+        else:
+            user['xp'] = new_xp
+
+        return user
+
