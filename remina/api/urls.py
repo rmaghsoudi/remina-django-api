cd47723b3cdaca5c9437c44181af45e9e4ed5b2c
Romy Maghsoudi <romy.mag9@gmail.com>
2021-03-05 03:56:45 -0500

    added new view to retrieve all User Todos

--- remina/api/urls.py
+++ remina/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
-from .views import UserView, TodoView, HabitView, GoalView, CheckView
+from .views import UserView, TodoView, TodoDetailView, HabitView, GoalView, CheckView
 
 
 urlpatterns = [
     path('users/', UserView.as_view()),
     path('users/<int:pk>', UserView.as_view()),
-    path('todos/', TodoView.as_view()),
-    path('todos/<int:pk>', TodoView.as_view()),
+    path('todos', TodoView.as_view()),
+    path('todos/<int:pk>', TodoDetailView.as_view()),
     path('habits/', HabitView.as_view()),
     path('habits/<int:pk>', HabitView.as_view()),
     path('goals/', GoalView.as_view()),
--- remina/api/views.py
+++ remina/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseServerError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -50,6 +50,20 @@ class UserView(APIView):
 
 class TodoView(APIView):
 
+    def get_objects(self):
+        try: 
+           return Todo.objects.filter(user=self.request.query_params.get('user_id'))   
+        except:
+            raise HttpResponseServerError
+
+    def get(self, request, format=None):
+        todos = self.get_objects()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+
+class TodoDetailView(APIView):
+
     def get_object(self, pk):
         try:
             return Todo.objects.get(pk=pk)
@@ -60,15 +74,6 @@ class TodoView(APIView):
         todo = self.get_object(pk)
         serializer = TodoSerializer(todo)
         return Response(serializer.data)
-
-    def post(self, request, format=None):
-        new_todo = request.data
-        serializer = TodoSerializer(data=new_todo)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk, format=None):
         todo = self.get_object(pk)
