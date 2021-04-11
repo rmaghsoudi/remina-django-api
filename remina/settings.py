d89e9006c7f675224e237991e40d869938e8f181
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-11 18:29:07 -0400

    adding default renderer classes

--- remina/settings.py
+++ remina/settings.py
@@ -90,6 +90,10 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
     'DATETIME_FORMAT': '%b %d, %Y %-I:%M %p'
 }
 # Database
