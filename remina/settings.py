daeae89f23c2b2873d6bb4f8138c8b3dcc1b0203
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-13 05:13:36 -0400

    adding prod db

--- remina/settings.py
+++ remina/settings.py
@@ -190,5 +190,7 @@ JWT_AUTH = {
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'] = dj_database_url.config(default=env('DATABASE_URL'))
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
