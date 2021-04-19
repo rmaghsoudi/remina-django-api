778280edbb7a97406e35daf12fa6e62875fe5317
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-19 03:16:26 -0400

    removing useless import

--- api/views.py
+++ api/views.py
@@ -13,7 +13,6 @@ from functools import wraps
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from coalesce import coalesce
 
 # Create your views here.
 
