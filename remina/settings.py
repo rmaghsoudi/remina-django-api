f642c5909b371df86f217cd35c4084e6bd26765d
Romy Maghsoudi <romy.mag9@gmail.com>
2021-05-15 20:26:02 -0400

    changing allowed hosts

--- remina/settings.py
+++ remina/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['remina-stage.herokuapp.com', 'remina-prod.herokuapp.com']
+ALLOWED_HOSTS = ['remina.herokuapp.com']
 
 
 # Application definition
@@ -192,9 +192,10 @@ JWT_AUTH = {
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 DATABASES['default'] = dj_database_url.config(default=env('DATABASE_URL'))
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
